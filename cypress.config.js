4983468f293f59d6742920139d3fb63f03f9f2b8
karina avetisyan <karina.avetisyan1@yandex.by>
2024-01-25 17:00:03 +0100

    fix projectId

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "qeq1s5",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
