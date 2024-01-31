21c0a926714bd5187904173d0b2491245073f9ea
karina avetisyan <karina.avetisyan1@yandex.by>
2024-01-31 15:51:16 +0100

    try split

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require("cypress");
 const { configureAllureAdapterPlugins } = require('@mmisty/cypress-allure-adapter/plugins');
+const cypressSplit = require('cypress-split')
+
 
 module.exports = defineConfig({
   env: {
@@ -10,6 +12,7 @@ module.exports = defineConfig({
       //console.log(config) // see everything in here!
 
       configureAllureAdapterPlugins(on, config);
+      cypressSplit(on, config)
       // modify config values
       config.defaultCommandTimeout = 10000;
       config.baseUrl = 'https://example.cypress.io';
