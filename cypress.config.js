1fdc2dbd192691377c7ecc82f471d87f31405d3f
karinaAv <koamdja.dow19dke@gmail.com>
2024-03-13 09:46:33 +0100

    attempt 2

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,26 @@
 const { defineConfig } = require("cypress");
-const { configureAllureAdapterPlugins } = require('@mmisty/cypress-allure-adapter/plugins');
-const cypressSplit = require('cypress-split')
-
+const {
+  configureAllureAdapterPlugins,
+} = require("@mmisty/cypress-allure-adapter/plugins");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
+const cypressSplit = require("cypress-split");
 
 module.exports = defineConfig({
   env: {
-    allure: true,
+    allureReuseAfterSpec: true,
   },
   e2e: {
     setupNodeEvents(on, config) {
       //console.log(config) // see everything in here!
 
-      configureAllureAdapterPlugins(on, config);
-      cypressSplit(on, config)
+      cypressSplit(on, config);
       // modify config values
       config.defaultCommandTimeout = 10000;
-      config.baseUrl = 'https://example.cypress.io';
+      config.baseUrl = "https://example.cypress.io";
+      allureWriter(on, config);
 
       // modify env var value
-      config.env.ENVIRONMENT = 'base';
+      config.env.ENVIRONMENT = "base";
 
       // IMPORTANT return the updated config object
       return config;
