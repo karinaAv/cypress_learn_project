a57b078623519c18e79d9e7b720392062f532b42
karinaAv <koamdja.dow19dke@gmail.com>
2024-03-26 16:10:49 +0100

    attempt 75

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,15 @@
 const { defineConfig } = require("cypress");
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
-
 const cypressSplit = require("cypress-split");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      allureWriter(on, config);
       cypressSplit(on, config);
       // modify config values
       config.defaultCommandTimeout = 10000;
       config.baseUrl = "https://example.cypress.io";
-
+      allureWriter(on, config);
       return config;
     },
     env: {
